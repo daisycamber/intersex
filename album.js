50bcc4f065c06f3ae6a2e544f9a697c8eb913795
Jasper Holton <jasper.camber.holton@gmail.com>
2019-06-07 22:22:56 -0700

    Update album.js

--- album.js
+++ album.js
@@ -1,6 +1,7 @@
-canvas.width = 3000
-canvas.height = 3000
+
 var c = document.getElementById("myCanvas");
+c.width = 3000
+c.height = 3000
 var ctx = c.getContext("2d");
 ctx.moveTo(0, 0);
 ctx.lineTo(3000, 3000);
