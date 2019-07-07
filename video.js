234f2e0469b5eb894cee2bfe8a30e03d2f938381
Jasper Holton <jasper@holton.io>
2019-07-07 12:15:48 -0700

    Update video.js

--- video.js
+++ video.js
@@ -66,7 +66,7 @@ function update ()
             rings[i].depth = rings.length - i;
             rings[i].set
         }
-        rings[Phaser.Math.Between(0,rings.length)].setFillStyle("0x"+Phaser.Math.Between(0x999999,0xFFFFFF).toString(16));
+        rings[Phaser.Math.Between(0,rings.length-1)].setFillStyle("0x"+Phaser.Math.Between(0x999999,0xFFFFFF).toString(16));
         lastBeat = frame;
     }
     else{
